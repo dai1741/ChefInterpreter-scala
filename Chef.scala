a9c5c79dde87261f532af095946a2efa6784c8e5
Dai Fujita <fujita.dai@gmail.com>
2012-11-10 19:01:57 +0900

    Refactor and fix bug around line separators

--- Chef.scala
+++ Chef.scala
@@ -3,8 +3,8 @@ package jp.dai1741.parsing.chef
 import scala.util.parsing.combinator._
 import scala.util.Random
 import scala.collection.mutable.{ ArrayBuffer, HashMap }
-import scala.collection.immutable.{ TreeMap }
 import scala.collection.{ TraversableOnce }
+import java.util.regex.Pattern
 
 // TODO: Support auxiliary recipe and verb loops
 class ChefParsers extends JavaTokenParsers {
@@ -18,29 +18,31 @@ class ChefParsers extends JavaTokenParsers {
   def レシピタイトル = 一行の文章 <~ 項目区切り
   def レシピコメント = "^(?!・材料)".r ~>一段落 <~ 項目区切り
   
-  var ingredients = new TreeMap[String, Ingredient]
+  var ingredientsParser: Parser[String] = failure("No ingredients available")
   def 材料表 = "・材料\n" ~> 材料定義.+ <~ 項目区切り ^^ { (ls) ⇒ {
-    ingredients = TreeMap(ls: _*)
+    ingredientsParser =
+      ("(?!)" :: ls.map(_._1).distinct.sortBy(-_.size).map(Pattern.quote)).mkString("|").r
     ls
   }}
   
-  def 材料定義 = 材料名 ~ ("：" ~> 分量).? <~ "\n" ^^ {
-    case (name ~ Some(Some(amount) ~ iType)) ⇒ (name → Ingredient(iType, amount, true))
-    case (name ~ Some(None ~ iType)) ⇒ (name → Ingredient(iType))
-    case (name ~ None) ⇒ (name → defaultIngredient.copy())
+  def 材料定義 = 材料名 ~ (("：" ~> 分量).? ^^ {
+    _.getOrElse(new ~(None, None))
+  }) <~ "\n" ^^ { case (name ~ (amount ~ iType)) ⇒
+    (name → Ingredient(iType.getOrElse(dry), amount.getOrElse(-1), amount.isDefined))
   }
   
   def 材料名 = """[^：\n]+""".r ^^ { _.trim() }
   
-  def 分量 = ("約".? ~> 前単位.? ~> 整数.? ~ (後単位.? ^^ {
-    _.getOrElse(defaultIngredient.iType)
-  })) ||| "適量|少々".r ^^^ new ~(None, defaultIngredient.iType)
+  def 分量 = (前単位.? ~> 整数.? ~ (後単位.? ^^ {
+    _.getOrElse(None)
+  })) ||| "適量|少々".r ^^^ new ~(None, None)
   
-  def 前単位 = "約|大さじ|小さじ".r
+  def 前単位 = "約?(大さじ|小さじ)?".r
   def 後単位 = (液状後単位 | 固形後単位 | 不明後単位) <~ "弱|強|(?:程度?)?".r
-  def 液状後単位 = "(?:滴|(ミリ)?リットル|杯|cc|ml|L)".r ^^^ liquid
-  def 固形後単位 = "(?:個|枚|(キロ)?グラム|切れ|かけ|片|玉|本|匹|束|袋|株|丁|cm|g|kg)".r ^^^ dry
-  def 不明後単位 = "(?:カップ|升|合|缶)".r ^^^ defaultIngredient.iType
+  def 液状後単位 = "(?:滴|(ミリ)?リットル|杯|cc|ml|L)".r ^^^ Some(liquid)
+  def 固形後単位 =
+    "(?:個|枚|(キロ)?グラム|切れ|かけ|片|玉|本|匹|束|袋|株|丁|cm|g|kg)".r ^^^ Some(dry)
+  def 不明後単位 = "(?:カップ|升|合|缶)".r ^^^ None
   
   def 料理時間 = "料理時間(の目安)?：".r ~> 整数 <~ 一行の文章 <~ 項目区切り
   
@@ -49,63 +51,39 @@ class ChefParsers extends JavaTokenParsers {
   def 作り方 = "・作り方\n" ~> (手順 <~ """。\n?|\n|\z""".r).+ <~ 項目区切り
   
   def 手順 =
-    冷蔵庫から取り出す ^^ { FromFridge(_) } |
-    ボウルに入れる ^^ { unpackOperation(_, Put) } |
-    ゆっくり混ぜる ^^ { unpackOperation(_, Fold) } |
-    ボウルに加える ^^ { unpackOperation(_, Add) } |
-    ボウルから取り出す ^^ { unpackOperation(_, Remove) } |
-    ボウルに入れて混ぜる ^^ { unpackOperation(_, Combine) } |
-    一部をボウルに入れる ^^ { unpackOperation(_, Divide) } |
-    固形物をボウルに加える ^^ { AddDries(_) } |
-    材料を液状にする ^^ { LiquefyIngredient(_) } |
-    ボウルを液状にする ^^ { LiquefyContents(_) } |
-    ボウルをかき混ぜる ^^ { unpackOperation(_, StirWhile) } |
-    かき混ぜながら入れる ^^ { unpackOperation(_, StirWith) } |
-    ボウルをよく混ぜる ^^ { Mix(_) } |
-    ボウルをきれいにする ^^ { Clean(_) } |
-    オーブン皿へ移す ^^ { unpackOperation(_, Pour) } |
-    // TODO: implement
-    何かする ^^^ Verb("hoge", None) |
-    何かし終わるまで何かする ^^^ VerbUntil(None, "hoge")|
-    添える ^^^ ServeWith("hoge") |
-    わきに置く ^^^ SetAside() |
-    冷蔵庫に入れる ^^ { Refrigerate(_) }
+    例の材料("を冷蔵庫から取り出す")                                  ^^ FromFridge |
+    材料をボウル("に") <~ "入れる"                                    ^^ composite(Put) |
+    材料をボウル("に") <~ "入れてゆっくり混ぜる"                      ^^ composite(Fold) |
+    材料をボウル("に") <~ "加える"                                    ^^ composite(Add) |
+    材料をボウル("から") <~ "取り出す"                                ^^ composite(Remove) |
+    材料をボウル("に") <~ "入れて混ぜる"                              ^^ composite(Combine) |
+    例の材料("を一部") ~ n番目のボウル("に") <~ "入れる"              ^^ composite(Divide) |
+    "固形物を全部" ~> n番目のボウル("に") <~ "加える"                 ^^ AddDries |
+    例の材料("を混ぜて液状にする")                                    ^^ LiquefyIngredient |
+    n番目の略せないボウル("の中身をよく混ぜて液状にする")             ^^ LiquefyContents |
+    n番目の略せないボウル("の中身を") ~ 整数 <~ "分間かき混ぜる"      ^^ composite(StirWhile) |
+    例の材料("をかき混ぜながら") ~ n番目のボウル("に") <~ "入れる"    ^^ composite(StirWith) |
+    n番目の略せないボウル("の中身をよく混ぜる")                       ^^ Mix |
+    n番目の略せないボウル("をきれいにする")                           ^^ Clean |
+    n番目の略せないボウル("の中身を") ~ n番目の <~ "オーブン皿へ移す" ^^ composite(Pour) |
+    // it parses 5 rules below, but they are not implemented so far
+    例の材料("を").? ~ どうする                                       ^^ composite(Verb) |
+    何かし終わるまで ~> 例の材料("を").? ~ どうする                   ^^ composite(VerbUntil) |
+    """[^\n。]+を添える(?=[\n。])""".r                                ^^ { (s) ⇒ 
+      ServeWith(s.replaceFirst("を添える$", ""))
+    } |
+    "しばらく置いておく"                                              ^^^ SetAside() |
+    (整数 <~ "時間").? <~ "冷蔵庫に入れておく"                        ^^ Refrigerate
   
-  def unpackOperation[A, B](o: ~[A, B], func: (A, B) ⇒ Operation) = o match {
+  def composite[A, B](func: (A, B) ⇒ Operation): ((~[A, B]) ⇒ Operation) = _ match {
     case (a ~ b) ⇒ func(a, b)
   }
   
-  def 配膳 = ((整数 <~ "人分|人前".r <~ 一行の文章.?) | ("さあ召し上がれ！" ^^^ 1)) ^^ {
-    Serve(_)
-  }
+  def 配膳 = ((整数 <~ "人分|人前".r <~ 一行の文章.?) | ("さあ召し上がれ！" ^^^ 1)) ^^ Serve
   
-  def 冷蔵庫から取り出す = 例の材料("を冷蔵庫から取り出す")
-  def ボウルに入れる = 材料をボウル("に") <~ "入れる" // Note that `材料をボウル("に入れる")` won't work, witch is optional
-  def ゆっくり混ぜる = 材料をボウル("に") <~ "入れてゆっくり混ぜる"
-  def ボウルに加える = 材料をボウル("に") <~ "加える"
-  def ボウルから取り出す = 材料をボウル("から") <~ "取り出す"
-  def ボウルに入れて混ぜる = 材料をボウル("に") <~ "入れて混ぜる"
-  def 一部をボウルに入れる = 例の材料("を一部") ~ n番目のボウル("に") <~ "入れる"
-  def 固形物をボウルに加える = "固形物を全部" ~> n番目のボウル("に") <~ "加える"
-  def 材料を液状にする = 例の材料("を混ぜて液状にする")
-  def ボウルを液状にする = n番目の略せないボウル("の中身をよく混ぜて液状にする")
-  def ボウルをかき混ぜる = n番目の略せないボウル("の中身を") ~ 整数 <~ "分間かき混ぜる"
-  def かき混ぜながら入れる = 例の材料("をかき混ぜながら") ~ n番目のボウル("に") <~ "入れる"
-  def ボウルをよく混ぜる = n番目の略せないボウル("の中身をよく混ぜる")
-  def ボウルをきれいにする = n番目の略せないボウル("をきれいにする")
-  def オーブン皿へ移す = n番目の略せないボウル("の中身を") ~ (n番目の <~ "オーブン皿へ移す")
-  
-  // it parses 5 rules below, but they are not implemented so far
-  def 何かする = 例の材料("を").? <~ どうする
-  def 何かし終わるまで何かする = 何かし終わるまで ~> 例の材料("を").? <~ どうする
-  def 添える = """[^\n]+を添える(?=[\n。])""".r ^^ { _.replaceFirst("を添える$", "") }
-  def わきに置く = "しばらく置いておく"
-  def 冷蔵庫に入れる = (整数 <~ "時間").? <~ "冷蔵庫に入れておく"
-  
-  def 例の材料(suf: String = "") =
-    // TODO: Stop build regex rule each time for efficiency
-    // Or rather, this implementaion is stupid. better to capture all string before detecting ingredient?
-    (Parser { ((ingredients.keys.toSeq.reverse mkString "|").r)(_) }) <~ suf
+  def 例の材料(suf: String = "") = ingredientsParser <~ suf
+  // this implementaion is somewhat stupid. better to capture all string before detecting ingredient?
+    
   def n番目の = ((整数 <~ "番目の").? ^^ {
     _.getOrElse(1) - 1
   })
@@ -124,7 +102,7 @@ class ChefParsers extends JavaTokenParsers {
   def 項目区切り = """\n+|\z""".r
   def 整数 = decimalNumber ^^ { _.toInt }
   
-  case class Ingredient(var iType: IngredientType = dry,
+  case class Ingredient(var iType: IngredientType,
                         private var _data: Int = -1,
                         var isDefined: Boolean = false) {
     def data_=(n: Int) = {
@@ -145,7 +123,6 @@ class ChefParsers extends JavaTokenParsers {
   case class IngredientType(convertData: (Int) ⇒ String)
   val dry = IngredientType(_.toString)
   val liquid = IngredientType(_.toChar.toString)
-  val defaultIngredient = Ingredient()
   
   class ChefStacks extends HashMap[Int, Stack[Ingredient]] {
     override def apply(i: Int) = {
@@ -170,8 +147,6 @@ class ChefParsers extends JavaTokenParsers {
     }
     def stir(num: Int) = insert(math.max(size - num - 1, 0), pop)
   }
-  val bowls = new ChefStacks
-  val dishes = new ChefStacks
   
   
   
@@ -194,7 +169,7 @@ class ChefParsers extends JavaTokenParsers {
   case class Mix(bowl: Int) extends Operation
   case class Clean(bowl: Int) extends Operation
   case class Pour(bowl: Int, dish: Int) extends Operation
-  case class Verb(verb: String, ingredient: Option[String]) extends Operation
+  case class Verb(ingredient: Option[String], verb: String) extends Operation
   case class VerbUntil(ingredient: Option[String], verbed: String) extends Operation
   case class SetAside() extends Operation
   case class ServeWith(recipe: String) extends Operation
@@ -205,13 +180,13 @@ class ChefParsers extends JavaTokenParsers {
   class ChefInterpreter {
     val bowls = new ChefStacks
     val dishes = new ChefStacks
-    var ingredients = new TreeMap[String, Ingredient]
+    var ingredients = new HashMap[String, Ingredient]
     
     implicit def string2ingredient(s: String): Ingredient = ingredients(s)  // this is dangerous
     
     def process(expr: Operation) = expr match {
       case FromFridge(ingred) ⇒ {
-        ingredients(ingred).data = readInt // probably Scanner#nextInt is better
+        ingred.data = readInt // probably Scanner#nextInt is better
       }
       case Put(ingred, bowl) ⇒ {
         bowls(bowl).push(ingred.copy())
@@ -285,16 +260,9 @@ class ChefParsers extends JavaTokenParsers {
 }
 
 class VerboseChefParsers extends ChefParsers {
-  override def 材料表 = super.材料表 ^^ { (x) ⇒
-    println("ingredients: " + ingredients)
-    x
-  }
   override def 材料定義 = log(super.材料定義)("材料定義")
   override def 材料名 = log(super.材料名)("材料名")
-  override def 手順 = log(super.手順)("手順") ^^ { (x) ⇒
-    println(bowls(0).toSeq.reverse.map(_.toString) mkString "")
-    x
-  }
+  override def 手順 = log(super.手順)("手順")
 }
 
 object ChefParsers {
