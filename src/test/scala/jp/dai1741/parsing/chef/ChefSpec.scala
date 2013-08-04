80df1978b9d6838a3c95320b6f296001585b4446
Dai Fujita <fujita.dai@gmail.com>
2013-08-04 23:05:48 +0900

    Add specs for original Chef interpreter
    I'm too lazy to test the parser functions

--- src/test/scala/jp/dai1741/parsing/chef/ChefSpec.scala
+++ src/test/scala/jp/dai1741/parsing/chef/ChefSpec.scala
@@ -14,20 +14,25 @@ object ChefSpec extends Specification {
     stream.toString
   }
   
-  def checkRecipe(filePath: String, expected: String) {
+  def checkRecipe(filePath: String, expected: String, evaler: (String ⇒ Unit)) {
     getStdOutIn {
-      Chef.evalJapanese(getResouceString(filePath))
+      evaler(getResouceString(filePath))
     }.replaceAll("""(?<=^|\n) | (?=$|\n)""", "") must be matching expected.r
   }
   
-  "Chef runtime" should {
+  def checkJapaneseRecipe = checkRecipe(_: String, _: String, Chef.evalJapanese)
+  def checkOriginalRecipe = checkRecipe(_: String, _: String, Chef.evalOriginal)
+  
+  val brokenFibonacciNumbers = "1 0 2 1 3 1 4 1 5 1 6 1 7 1 8 1 9 1 10 1 11 1 12 1 13 1 14 1 15 1 16 1 17 1 18 1 19 1 20 1 21 1 22 1 23 1 24 1 25 1 26 1 27 1 28 1 29 1 30 1 31 1 32 1 33 1 34 1 35 1 36 1 37 1 38 1 39 1 40 1 41 1 42 1 43 1 44 1 45 1 46 1 47 1 48 1 49 1 50 1 51 1 52 1 53 1 54 1 55 1 56 1 57 1 58 1 59 1 60 1 61 1 62 1 63 1 64 1 65 1 66 1 67 1 68 1 69 1 70 1 71 1 72 1 73 1 74 1 75 1 76 1 77 1 78 1 79 1 80 1 81 1 82 1 83 1 84 1 85 1 86 1 87 1 88 1 89 1 90 1 91 1 92 1 93 1 94 1 95 1 96 1 97 1 98 1 99 1 1"
+
+  "Chef runtime with Japanese syntax" should {
   
     "output 'Hello world!' when evaluating 'chef_hello_jp.txt'" in {
-      checkRecipe("/chef_hello_jp.txt", "Hello world!")
+      checkJapaneseRecipe("/chef_hello_jp.txt", "Hello world!")
     }
     
     "output broken fibonacci numbers when evaluating 'chef_fib_jp.txt'" in {
-      checkRecipe("/chef_fib_jp.txt", "1 0 2 1 3 1 4 1 5 1 6 1 7 1 8 1 9 1 10 1 11 1 12 1 13 1 14 1 15 1 16 1 17 1 18 1 19 1 20 1 21 1 22 1 23 1 24 1 25 1 26 1 27 1 28 1 29 1 30 1 31 1 32 1 33 1 34 1 35 1 36 1 37 1 38 1 39 1 40 1 41 1 42 1 43 1 44 1 45 1 46 1 47 1 48 1 49 1 50 1 51 1 52 1 53 1 54 1 55 1 56 1 57 1 58 1 59 1 60 1 61 1 62 1 63 1 64 1 65 1 66 1 67 1 68 1 69 1 70 1 71 1 72 1 73 1 74 1 75 1 76 1 77 1 78 1 79 1 80 1 81 1 82 1 83 1 84 1 85 1 86 1 87 1 88 1 89 1 90 1 91 1 92 1 93 1 94 1 95 1 96 1 97 1 98 1 99 1 1")
+      checkJapaneseRecipe("/chef_fib_jp.txt", brokenFibonacciNumbers)
     }
     
     "output correct data for some samples" in {
@@ -38,13 +43,41 @@ object ChefSpec extends Specification {
         ("/sample_count10.txt", """1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n"""),
         ("/sample_instruments.txt", """([1259d ]+\n){2}1 55 1 1\n222""")
       ).foreach { case (recipe, expected) ⇒
-        checkRecipe(recipe, expected)
+        checkJapaneseRecipe(recipe, expected)
       }
       
       Console.withIn(new StringReader("15\n230\n")) {
-        checkRecipe("/sample_io.txt", "245")
+        checkJapaneseRecipe("/sample_io.txt", "245")
       }
     }
+  }
+  
+  // TODO: tidy up
+  "Chef runtime with Original syntax" should {
+    "output 'Hello world!' when evaluating 'chef_hello_original.txt'" in {
+      checkOriginalRecipe("/chef_hello_original.txt", "Hello world!")
+    }
+    
+    "output broken fibonacci numbers when evaluating 'chef_fib_original.txt'" in {
+      checkOriginalRecipe("/chef_fib_original.txt", brokenFibonacciNumbers)
+    }
+    
+    "output correct data for some samples" in {
+      List(
+        ("/sample_auxiliary_original.txt", "10 10 10"),
+        ("/sample_operations_original.txt", "3 891 1180"),
+        ("/sample_loops_original.txt", "996"),
+        ("/sample_count10_original.txt", """1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n"""),
+        ("/sample_instruments_original.txt", """([1259d ]+\n){2}1 55 1 1\n222""")
+      ).foreach { case (recipe, expected) ⇒
+        checkOriginalRecipe(recipe, expected)
+      }
+      
+      Console.withIn(new StringReader("15\n230\n")) {
+        checkOriginalRecipe("/sample_io_original.txt", "245")
+      }
+    }
+    
   }
 
 }
